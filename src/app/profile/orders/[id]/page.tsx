2f05578372bacc193aec9963cbaf69097e95e684
unknown <shayan.iker@gmail.com>
2025-12-07 11:31:11 +0330

    change order detail

--- src/app/profile/orders/[id]/page.tsx
+++ src/app/profile/orders/[id]/page.tsx
@@ -8,22 +8,26 @@ import OrderStatus from "./OrderStatus";
 async function Page({ params }) {
   const { id } = params;
   const data = await getMyOrderServerComponent();
-  const { orders } = data.data;
+  const { orders } = data.data.data;
   const theOrder = orders?.find((order) => order._id === id);
-
   return (
     <Suspense fallback={<Loading />}>
-      <div className="flex h-full flex-col gap-8 rounded-lg border border-light-200 px-5 py-6 shadow-container">
-        <div id="order-status-container">
-          <OrderStatus theOrder={theOrder} />
+      {theOrder && (
+        <div className="flex h-full flex-col gap-8 rounded-lg border border-light-200 px-5 py-6 shadow-container">
+          <div id="order-status-container">
+            <OrderStatus theOrder={theOrder} />
+          </div>
+          <div id="order-detail-container">
+            <OrderDetail theOrder={theOrder} />
+          </div>
+          <div id="order-address-container">
+            <OrderAddress theOrder={theOrder} />
+          </div>
         </div>
-        <div id="order-detail-container">
-          <OrderDetail theOrder={theOrder} />
-        </div>
-        <div id="order-address-container">
-          <OrderAddress theOrder={theOrder} />
-        </div>
-      </div>
+      )}
+      {!theOrder && (
+        <div className="flex justify-center">سفارش مورد نظر یافت نشد</div>
+      )}
     </Suspense>
   );
 }
